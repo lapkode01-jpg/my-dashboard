7e2a688e0f8d70e85fb6d54f0c0db2583746d1ca
lapkode01-jpg <lapkode01@gmail.com>
2026-09-16 22:21:49 +0300

    Add files via upload

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.responses import FileResponse
+from fastapi.responses import FileResponse
 from starlette.staticfiles import StaticFiles
 
 from backend.schemas import ActionsSchema
